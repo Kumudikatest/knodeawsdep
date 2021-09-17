60e588223c4a607cf4d96c52086c62648238e8a7
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2021-09-17 17:29:31 +0530

    test

---
+++ knodeawsdep.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    debugger;
+    try {
+        let data = await ddb.scan({
+            TableName: "KChineseAnimal"
+        }).promise();
+        console.log(data);
+
+    } catch (err) {
+        console.log(err);
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
+};
